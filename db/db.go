9380b67f6b2870ac1831a84eb6859c113a731fc9
Lev Nemirovskii <levnemir@yandex.ru>
2024-04-29 19:23:15 +0300

    Change go lint

--- db/db.go
+++ db/db.go
@@ -4,13 +4,13 @@ import (
 	"github.com/nemirlev/zenapi"
 )
 
-// DB это интерфейс для базы данных. Методы специфичны для работы с данными ДзенМани
+// DB это интерфейс для базы данных. Методы специфичны для работы с данными ДзенМани.
 type DB interface {
 	Save(data *zenapi.Response) error
 }
 
-// DatabaseEntity это интерфейс для сущностей в БД. Само описания сущностей в /internal/models
-//type DatabaseEntity interface {
+//	DatabaseEntity это интерфейс для сущностей в БД. Само описания сущностей в /internal/models
+//	type DatabaseEntity interface {
 //	GetTableName() string
 //	GetInsertQuery() string
 //	GetValues() []interface{}
